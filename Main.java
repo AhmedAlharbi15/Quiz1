4c5afe186b5697b0333577d6076c0b9555a90eb2
AhmedAlharbi15 <159686025+AhmedAlharbi15@users.noreply.github.com>
2024-02-19 11:37:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner in = new Scanner(System.in);
+//   Q1:
+//        System.out.Print("Please Number 1");
+//        int number1 = in.nextInt();
+//        System.out.Print("Please Number 2");
+//        int number2 = in.nextInt();
+//        System.out.Print("Please Number 3");
+//        int number3= in.nextInt();
+//        if (number1 > number2) {
+//            System.out.Print(number1);
+//            else if (number1>number3){
+//                System.out.Print(number1);
+//                else if (number2>number1){
+//                    System.out.Print(number2);
+//                    else if (number2>number3)
+//                        System.out.Print(number2);
+//                    else if (number3>number1)
+//                        System.out.Print(number3);
+//                    else if (number3>number2)
+//                        System.out.Print(number3)
+//                }
+//            }
+//        }
+//     Q2:
+//        System.out.Print("Please Number");
+//        int number =in.nextInt();
+//
+//        StringBuilder nu =new StringBuilder(number);
+//
+//        System.out.Print(number+nu);
+
+//      Q3:
+//        System.out.Print("Please Number");
+//        int Number = in.nextInt();
+//        for (int Number = 0; Number < 10; i++) {
+//            System.out.Print(Number);
+
+
+//       Q4:
+//        int[] Number={10,-21,30,31,-25};
+//        if (Number > 0) {
+//            System.out.Prints("positive :"+ Number);
+//            else if (Number < 0){
+//                System.out.Print("negative :" Number);
+//        }
+//        }
+
+
+//      Q5:
+//      String[] ar={"Tuwaiq","Java", "Student","Bootcamp"};
+//        int index1= ar.length;
+//        if (ar.length == 4) {
+//            System.out.Print(ar.length);
+//        }
+//      }
+    }
